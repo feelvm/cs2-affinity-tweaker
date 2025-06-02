32d2a8bc8a977840b2612decb072db84f1a896e5
Stanislav <84909845+feelvm@users.noreply.github.com>
2025-06-02 21:26:27 +0200

    Update affinity.bat

--- affinity.bat
+++ affinity.bat
@@ -3,15 +3,11 @@
 :WAITFOR_CS2
 for /f "tokens=2" %%i in ('tasklist ^| findstr /i "cs2.exe"') do set PID=%%i
 if "%PID%"=="" (
-    echo cs2.exe is not running. Please start cs2.exe and then run this script.
+    echo cs2.exe is not running
     pause
     exit /b 1
 )
 
 powershell -Command "& {Get-Process -Id %PID% | ForEach-Object { $_.ProcessorAffinity=0x000000000000FFFE }}"
-
-taskkill /f /im nordvpn-service.exe
-taskkill /f /im mysql.exe
-taskkill /f /im mysqld.exe 
 
 exit
